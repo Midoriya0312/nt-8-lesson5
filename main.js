73e73575081d582ea63ed32de81e6a8245fb3fb1
Midoriya0312 <oybektojiboyev0312@gmail.com>
2023-06-27 21:12:35 +0500

    lesson5

---
+++ main.js
@@ -0,0 +1,60 @@
+// let pi = Math.PI;
+
+// console.log(pi);
+// let num = 4.6;
+
+// let res = Math.round(num);
+
+// let random = Math.random(4)
+
+
+// console.log(random);
+
+
+// let random = Math.floor(Math.random() * 50);
+
+// console.log(random );
+
+// let instagramusers = ['ali', 'vali', 'gani', 'qani'];
+
+
+// let random = Math.floor(Math.random() * instagramusers.length);
+
+// let randomUsers = instagramusers[random];
+
+// console.log(randomUsers);
+
+
+// Homework 
+
+
+// 1 misol
+
+// let text = 'adsjfdsfssfjsdjhacabcsbajda';
+
+// function check(value){
+//     for(let i = 0; i < value.length; i++){
+//             if(value[i] === 'b'){
+//                 console.log('b');
+//             }
+//     }
+// }
+
+// check(text)
+
+// 2 misol
+
+// let text = 'cbcbdde';
+
+// console.log(text.slice(6));
+
+// 3 misol
+
+
+
+
+
+// 5 misol
+
+// let arr = ["You", "are", "beautiful", "looking"];
+
